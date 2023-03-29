d2f8e3b053d8915d89528e7f516e1924da23dfa9
cuteboyqq <58428559+cuteboyqq@users.noreply.github.com>
2023-03-29 19:33:56 +0800

    Add files via upload

--- execute14.py
+++ execute14.py
@@ -430,19 +430,20 @@ class MainWindow(QMainWindow):
     
     def run_command(self):
         now = datetime.now()
-        s_time = datetime.strftime(now,'%y-%m-%d_%H:%M:%S')
+        s_time = datetime.strftime(now,'%y-%m-%d_%H-%M-%S')
         str_s_time = str(s_time)
         source_type = self.source_combo.currentText()
         view_img = "--view-img" if self.viewimg_checkbox.isChecked() else ""
         save_airesult = "--save-airesult" if self.saveairesult_checkbox.isChecked() else ""
-        #weights = "--weights runs/train/f192_3cls_Argos_2023-03-11/weights/best.pt"
+        #weights = "--weights runs/tr＃ain/f192_3cls_Argos_2023-03-11/weights/best.pt"
         weights = "--weights runs/train/f192-4cls-Argos-2023-03-20/weights/best.pt"
         img_size = "--img-size 192"
         data = "--data data/factory_new2.yaml"
         save_folder_name = "--name "+str_s_time
         #save_folder_name = ""
         #video_path = "/home/ali/factory_video/ori_video_ver2.mp4"
-        detect_file = "detect-ori-log.py"
+        #detect_file = "detect-ori-log.py"
+        detect_file = "detect-simple-log.py"
         if source_type == "Camera":
             command = f"python {detect_file} --source 0 {weights} {img_size} {data} {view_img} {save_folder_name} {save_airesult}"
         elif source_type == "Video":
